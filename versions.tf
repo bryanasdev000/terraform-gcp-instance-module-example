a3a82f35d5826b207952996b6285875eb7ead2ce
Bryan A. S <53131727+bryanasdev000@users.noreply.github.com>
2020-09-21 20:47:10 -0300

    versions.tf: Version bump to terraform v0.13

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 # Configs do Terraform
 terraform {
   # Versao do Terraform e Provider (GCP)
-  required_version = "~> 0.12.0"
+  required_version = "~> 0.13.0"
   required_providers {
-    google = "~> 2.12"
+    google = "~> 3.0"
   }
   # Habilita a validacao de variaveis (experimental)
-  experiments = [variable_validation]
+  # experiments = [variable_validation]
 }
